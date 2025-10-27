6b10bb4aff09a3d0fcb5f357f80763fc51fde61a
DESKTOP-3LHBG1B\usuario_tarde <alejandromonterodeltoro@gmail.com>
2025-10-27 18:11:57 +0100

    HotelAlixar

---
+++ src/main/java/org/iesalixar/daw2/hotelalixar/HotelalixarApplication.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.hotelalixar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelalixarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelalixarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iesalixar/daw2/hotelalixar/HotelalixarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.hotelalixar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelalixarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
