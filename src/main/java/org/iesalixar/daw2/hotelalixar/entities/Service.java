3efbd837f49b16e173f45fb17e54b614935e1fb4
DESKTOP-3LHBG1B\usuario_tarde <alejandromonterodeltoro@gmail.com>
2025-10-28 17:28:08 +0100

    Actualizado Service Entities

--- src/main/java/org/iesalixar/daw2/hotelalixar/entities/Service.java
+++ src/main/java/org/iesalixar/daw2/hotelalixar/entities/Service.java
@@ -1,5 +1,8 @@
 package org.iesalixar.daw2.hotelalixar.entities;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,9 +16,12 @@ public class Service {
     private String service_name;
     private String description;
     private double price;
-    private int employee_id;
 
-    public Service(String service_name, String description, double price, int employee_id) {
+    @OneToMany
+    @JoinColumn(name = "employee_id") // FK hacia la tabla rooms
+    private Employee employee_id;
+
+    public Service(String service_name, String description, double price, Employee employee_id) {
         this.service_name = service_name;
         this.description = description;
         this.price = price;
